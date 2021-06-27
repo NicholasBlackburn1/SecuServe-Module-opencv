78ffa4670ff6eb86550c1d7da9271f527fbe54a2
nicholasblackburn1 <nickblackburn02@gmail.com>
2021-06-27 09:23:57 -0400

    adding knn class testing and data structure

---
+++ tests/test_knn_clasifiyer.py
@@ -0,0 +1,15 @@
+""" 
+this file is for testing the datasturue for handing faces
+#author Nicholas 
+"""
+import unittest
+from videoprocessingsrc import knnClasifiyer
+
+class TestKnnClass(unittest.TestCase):
+    
+    """
+    This is for only testing if data structure is valid
+    """
+    def test_training_knn_clasifiyer(self):
+        pass
+
