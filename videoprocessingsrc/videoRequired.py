a7692b20e028316e90e28d1b5b6010597bcb6f29
nicholasblackburn1 <nickblackburn02@gmail.com>
2021-07-07 13:02:18 -0400

    added signal strings to seperate messages

--- videoprocessingsrc/videoRequired.py
+++ videoprocessingsrc/videoRequired.py
@@ -12,6 +12,7 @@ class RequiredCode(object):
     # this allows me to set up pipe line easyerly  but for the cv module
     def setupPipeline(self,sender):
         #this sends a stats message back to the main controller and to the messaging and webserver modules
+        sender.send_string("PIPELINE")
         sender.send_json({"status":"starting","pipelinePos":"settingUp","time":__init__.datetime.now()})
         
         pipeline_start_setup = __init__.datetime.now()
@@ -41,11 +42,13 @@ class RequiredCode(object):
 
         __init__.console_Log.PipeLine_Ok("PipeLine Setup End time"+str( __init__.datetime.now() -  pipeline_start_setup))
         # updates stats message 
+        sender.send_string("PIPELINE")
         sender.send_json({"status":"finished","pipelinePos":"settingUp","time":str( __init__.datetime.now() -  pipeline_start_setup)})
         self.trainPipeLine(sender)
             
     # This trains the face model for the  pipeline
     def trainPipeLine(self,sender):
+        sender.send_string("PIPELINE")
         sender.send_json({"status":"starting","pipelinePos":"Training Model", "time": str(__init__.datetime.now())})
         pipeline_train_knn = __init__.datetime.now()
         __init__.console_Log.Warning("Training Model Going to take a while UwU..... ")
@@ -54,7 +57,8 @@ class RequiredCode(object):
         
         __init__.console_Log.PipeLine_Ok("Done Train Knn pipeline timer" + str(__init__.datetime.now() - pipeline_train_knn))
         __init__.console_Log.Warning("Done Training Model.....")
-        sender.send_json({"status":"starting","pipelinePos":" Done Training Model", "time": str(__init__.datetime.now() - pipeline_train_knn)})
+        sender.send_string("PIPELINE")
+        sender.send_json({"status":"done","pipelinePos":" Done Training Model", "time": str(__init__.datetime.now() - pipeline_train_knn)})
         
         # cleans mess as we keep prosessing
         __init__.gc.collect()
@@ -62,6 +66,7 @@ class RequiredCode(object):
         
         
     def reconitionPipeline(self,sender):
+        sender.send_string("PIPELINE")
         sender.send_json({"status":"starting","pipelinePos":"Starting reconition", "time": str(__init__.datetime.now())})
           # Camera Stream gst setup
         gst_str = ("rtspsrc location={} latency={}  ! rtph264depay  ! nvv4l2decoder ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert !appsink".format(
@@ -110,6 +115,7 @@ class RequiredCode(object):
                     print(name)
 
                     if(name != None):
+                        sender.send_string("PIPELINE")
                         sender.send_json({"status":"in Face Rec","pipelinePos":"reconizing people", "time": str(__init__.datetime.now())})
                         if(name == 'unknown' and status == None):
                             __init__.userStats.userUnknown(__init__.const.opencvconfig, name, frame, font, imagename=self.imagename, imagePath=self.imagePath,
@@ -197,6 +203,7 @@ class RequiredCode(object):
                         return
 
             else:
+                sender.send_string("PIPELINE")
                 sender.send_json({"status":"ending","pipelinePos":"done pipeline", "time": str(__init__.datetime.now())})
                 exit(1001)
             return
