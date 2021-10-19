56c46a50c60864982ccfdcdfe1ac9e20a6834788
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-10-19 11:10:03 -0400

    Updated loggign output to be like dmsg

--- SecuServe-Module-opencv/util/consoleLog.py
+++ SecuServe-Module-opencv/util/consoleLog.py
@@ -8,38 +8,38 @@ from datetime import datetime
 
 
 def Debug(text):
-    dmsgLayout("DEBUG",text)
+    dmsgLayout("DEBUG",imports.Fore.LIGHTBLUE_EX,text)
     return
 
 
 def Warning(text):
-    dmsgLayout("WARNING",text)
+    dmsgLayout("WARNING",imports.Fore.YELLOW,text)
     return
 
 
 def Error(text):
-    dmsgLayout("ERROR",text)
+    dmsgLayout("ERROR",imports.Fore.RED,text)
     return
 
 
 def PipeLine_Ok(text):
-    dmsgLayout("OK",text)
+    dmsgLayout("OK",imports.Fore.LIGHTGREEN_EX,text)
     return
 
 
 def PipeLine_init(text):
-    dmsgLayout("INIT",text)
+    dmsgLayout("INIT",imports.Fore.LIGHTMAGENTA_EX,text)
     return
 
 
 def PipeLine_Data(text):
-    dmsgLayout("DATA",text)
+    dmsgLayout("DATA",imports.Fore.LIGHTCYAN_EX,text)
 
     return
 
 
-def dmsgLayout(type,message):
-    print(imports.Fore.GREEN + "["+str(datetime.now())+"]"+" "+ imports.Fore.YELLOW + str(type)+":"+" "+imports.Fore.WHITE+str(message))
+def dmsgLayout(type,color,message):
+    print(imports.Fore.GREEN + "["+str(datetime.now())+"]"+" "+ color + str(type)+":"+" "+imports.Fore.WHITE+str(message))
     print(imports.Style.RESET_ALL)
 
     
