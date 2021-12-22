ff65aeb7a92dbacd74aa775c004133630956e84b
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-12-22 07:29:53 -0500

    added title change on terminal

--- dev.sh
+++ dev.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+PS1=$
+PROMPT_COMMAND=
+echo -en "\033]0;Opencv Backend\a"
+
 echo "setting env vars"
 source  /home/nicky/.cache/pypoetry/virtualenvs/secuserve-module-opencv-_X9nZv5C-py3.8/bin/activate
 
