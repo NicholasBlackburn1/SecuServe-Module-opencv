6c73ba999861668b293498ae209882b348ba1bfd
NicholasBlackburn1 <nickblackburn02@gmail.com>
2023-01-09 17:40:20 -0500

    working on reriting dtabse connector usinhg api erver

--- SecuServe-Module-opencv/util/database.py
+++ SecuServe-Module-opencv/util/database.py
@@ -26,24 +26,22 @@ import requests
 ###########
 
 # Gets the Face Data from the Face data
-
-# TODO: NEED TO ONLY USE LIFE TIME DATABASE FOR FACES
-local = "/Documents/SECUSERVE/SecuServeFiles/"
-
 #! this section is for handling all the db interactions with the api server
 def getFaces():
     result_set =requests.get(url="http://127.0.0.1:2020/secuserve/api/v1.0/database/faces/getallfaces")
     consoleLog.PipeLine_Data("data from daabase"+ str(result_set.json()))
     return result_set.json()
 
-# gwts user uuid 
-def getUserid(face_data):
-    consoleLog.info('user id:'+ str(face_data[1]))
-    return str(face_data[1])
+#! gets users from user db
+def getUsers(face_data):
+    result_set =requests.get(url="http://127.0.0.1:2020/secuserve/api/v1.0/database/users/getallusers")
+    consoleLog.PipeLine_Data("users from daabase"+ str(result_set.json()))
+    return result_set.json()
 
 
-def getStatus(face_data):
-    pass
+def getStatus(usr_data):
+    consoleLog.Warning('User status is'+ str(usr_data[2]) + " for user"+ str(usr_data[1]))
+    return usr_data[2]
 
 
 #! make sure to rewrite sll the data flow to ue new db method instead of dirty db (reding from db directly)"
@@ -88,41 +86,3 @@ def getID(result_set, i):
     # gets Database entry name
 
 
-def getName(result_set, i):
-    id, useruuid, user, group, image, imageurl, phoneNum = result_set[i]
-    return user
-
-
-def getStatus(result_set, i):
-    id, useruuid, user, group, image, imageurl, phoneNum = result_set[i]
-    return group
-
-
-def getImageName(result_set, i):
-    id, useruuid, user, group, image, imageurl, phoneNum = result_set[i]
-    return image
-
-
-def getImageUrI(result_set, i):
-    id, useruuid, user, group, image, imageurl, phoneNum = result_set[i]
-    return imageurl
-
-
-def getUserUUID(result_set, i):
-    id, useruuid, user, group, image, imageurl, phoneNum = result_set[i]
-    return useruuid
-
-
-def getPhoneNum(result_set, i):
-    id, useruuid, user, group, image, imageurl, phoneNum = result_set[i]
-    return phoneNum
-
-
-def getLifefaces(result_set):
-    id, seenFaces, seenPlates, seenReconized, s
-from sqlalchemy import orm
-
-
-def getLifePlates(result_set, i):
-    id, seenFaces, seenPlates, seenReconized, seenUnReconized = result_set[i]
-    return seenPlates
